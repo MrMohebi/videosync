9b31db55cd77004e960d8ee98008de54d1207cd8
Rafael Ketsetsides <rketsetsides@gmail.com>
2020-06-24 15:57:44 +0300

    Removed hosts permission

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,21 +1,21 @@
 const select_video_button = document.getElementById("select_video"),
+    join_room_button = document.getElementById("join_room"),
     leave_room_button = document.getElementById("leave_room"),
-    create_room_button = document.getElementById("create_room"),
     share_page_button = document.getElementById("share_page"),
     username_input = document.getElementById("username"),
-    room_name_span = document.getElementById("room_name");
+    room_name_input = document.getElementById("room_name");
 
-leave_room_button.addEventListener("click",
-    () => browser.runtime.sendMessage({leave_room: true})
+join_room_button.addEventListener("click",
+    () => browser.storage.local.get("server")
+        .then(res => res.server)
+        .then(server => browser.runtime.sendMessage({
+            join_room: {room: room_name_input.value, server: server}
+        }))
         .then(() => window.close())
 );
 
-create_room_button.addEventListener("click",
-    () => browser.storage.local.get("server")
-        .then(res => res.server)
-        .then(server => browser.tabs.create({
-            url: "https://" + server + "/" + [...Array(10)].map(() => Math.random().toString(36)[2]).join("")
-        }))
+leave_room_button.addEventListener("click",
+    () => browser.runtime.sendMessage({leave_room: true})
         .then(() => window.close())
 );
 
@@ -23,12 +23,15 @@ share_page_button.addEventListener("click",
     () => browser.tabs.query({active: true, currentWindow: true})
         .then(tabs => tabs[0].url)
         .then(url => browser.runtime.sendMessage({share_url: url}))
+        .then(() => window.close())
 );
 
 select_video_button.addEventListener("click",
     () => browser.tabs.query({active: true, currentWindow: true})
         .then(tabs => tabs[0].id)
-        .then(tabid => browser.runtime.sendMessage({select_video: {tabid: tabid}})));
+        .then(tabid => browser.runtime.sendMessage({select_video: {tabid: tabid}}))
+        .then(() => window.close())
+);
 
 username_input.addEventListener("input",
     () => browser.storage.local.set({username: username_input.value})
@@ -39,10 +42,12 @@ browser.runtime.sendMessage({get_room: {properties: ["path"]}})
         if (room == null || room.path == null) {
             share_page_button.disabled = leave_room_button.disabled = select_video_button.disabled = true;
             share_page_button.style.display = leave_room_button.style.display = select_video_button.style.display = "none";
-            room_name_span.innerText = "(none)";
+            room_name_input.value = [...Array(10)].map(() => Math.random().toString(36)[2]).join("");
         }
         else {
-            room_name_span.innerText = room.path;
+            room_name_input.value = room.path;
+            room_name_input.disabled = join_room_button.disabled = true;
+            join_room_button.style.display = "none";
         }
     });
 
